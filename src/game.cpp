9877cde8534180f2b7957cdf86aadc6a168df3b9
Sam Veras <samuel.bernardo.veras@gmail.com>
2024-06-12 14:15:19 -0300

    Criação de N pêndulos com parametros carregados de arquivo

--- src/game.cpp
+++ src/game.cpp
@@ -1,9 +1,33 @@
 #include "game.hpp"
 #include <cmath>
 #include "classes.hpp"
+#include "functions.hpp"
 #include "raylib.h"
 
+void create_pendulums(Game& game, const Config& settings) {
+  for (size_t i = 0; i < settings.count; i++) {
+    float   p, t1, t2, m1, m2, l1, l2, d;
+    Color   c1, c2;
+    Vector2 o;
+
+    p  = float(i) / settings.count;
+    o  = settings.origin;
+    m1 = settings.mass_1, m2 = settings.mass_2;
+    l1 = settings.length_1, l2 = settings.length_2;
+    d  = settings.damping;
+    t1 = angle_interpolation(settings.initial_theta_1, settings.final_theta_1, p);
+    t2 = angle_interpolation(settings.initial_theta_2, settings.final_theta_2, p);
+    c1 = color_interpolation(settings.initial_color_1, settings.final_color_1, p);
+    c2 = color_interpolation(settings.initial_color_2, settings.final_color_2, p);
+
+    auto P = std::make_unique<DoublePendulum>(o, l1, m1, t1, c1, l2, m2, t2, c2, d);
+
+    game.add_drawable(std::move(P));
+  }
+}
+
 Game::Game() {
+  create_pendulums(*this, settings);
   SetConfigFlags(FLAG_MSAA_4X_HINT);
   InitWindow(settings.win_width, settings.win_height, settings.title.c_str());
   SetTargetFPS(settings.framerate);
