84389eda273c997544934ab9d7d03cd0c5dd9236
Sam Veras <samuel.bernardo.veras@gmail.com>
2024-06-12 15:35:07 -0300

    Extração de config class para seu próprio arquivo

---
+++ include/config.hpp
@@ -0,0 +1,30 @@
+#pragma once
+#include <raylib.h>
+#include <string>
+
+class Config {
+ public:
+  std::string title;
+  size_t      win_width, win_height, framerate;
+  Color       background_color;
+
+  Vector2 origin;
+  size_t  count;
+  float   damping;
+
+  int   length_1, length_2;
+  float mass_1, mass_2;
+  float initial_theta_1, initial_theta_2;
+  float final_theta_1, final_theta_2;
+  Color initial_color_1, initial_color_2;
+  Color final_color_1, final_color_2;
+
+  bool        show_fps, debug_mode;
+  std::string font_path;
+  float       font_size;
+  Font        font;
+
+  void init_font();
+  Config();
+  ~Config() = default;
+};
